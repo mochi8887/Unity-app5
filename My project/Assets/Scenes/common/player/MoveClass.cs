65a6be9d0a95c9a1447cec674854c42c46ec83f7
mochi8887 <asuto4573@gmail.com>
2026-03-13 21:15:27 +0900

    矢印修正

---
+++ My project/Assets/Scenes/common/player/MoveClass.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    public float speed = 3f;
+    private float playerSpeed;
+
+    Rigidbody2D rigidbody2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // ���L�[���������獶�����֐i��
+        if (Input.GetKey(KeyCode.LeftArrow)) playerSpeed = -speed;
+        // �E�L�[����������E�����֐i��
+        else if (Input.GetKey(KeyCode.RightArrow)) playerSpeed = speed;
+        // ���������Ȃ�������~�܂�
+        else playerSpeed = 0;
+
+        rigidbody2D.linearVelocity = new Vector2(playerSpeed, rigidbody2D.linearVelocity.y);
+    }
+}
